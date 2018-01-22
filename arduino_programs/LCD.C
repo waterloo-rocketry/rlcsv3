2a7e5aa60a9e02e6c665af9fd6dd154fae7c09f6
akmorrison <akmorris@edu.uwaterloo.ca>
2018-01-22 11:34:07 -0500

    Change last_daq to static variable, instead of undeclared pointer

--- arduino_programs/LCD.C
+++ arduino_programs/LCD.C
@@ -4,32 +4,33 @@ void lcd_nprint(int x, int y, int length, char *str);
 void lcd_print(int x, int y, char c);
 void output_labels();
 #include <string.h> 
-daq_holder_t* last_daq;
+
+static daq_holder_t last_daq;
 
 void lcd_init(LiquidCrystal lcd)
 {
 //just put in some default values to the buffered holders
-    last_daq->pressure1           = 0;
-    last_daq->pressure2           = 0;
-    last_daq->pressure3           = 0;
-    last_daq->pressure2           = 0;
-    last_daq->rocket_mass         = 0;
-    last_daq->ign_pri_current     = 0;
-    last_daq->ign_sec_current     = 0;
+    last_daq.pressure1           = 0;
+    last_daq.pressure2           = 0;
+    last_daq.pressure3           = 0;
+    last_daq.pressure2           = 0;
+    last_daq.rocket_mass         = 0;
+    last_daq.ign_pri_current     = 0;
+    last_daq.ign_sec_current     = 0;
    
-    last_daq->rfill_current_open  = 0;
-    last_daq->rfill_current_close = 0;
-    last_daq->rvent_current_open  = 0;
-    last_daq->rvent_current_close = 0;
-    last_daq->rfill_lsw_open      = 0;
-    last_daq->rfill_lsw_closed    = 0;
-    last_daq->rvent_lsw_open      = 0;
-    last_daq->rvent_lsw_closed    = 0;
+    last_daq.rfill_current_open  = 0;
+    last_daq.rfill_current_close = 0;
+    last_daq.rvent_current_open  = 0;
+    last_daq.rvent_current_close = 0;
+    last_daq.rfill_lsw_open      = 0;
+    last_daq.rfill_lsw_closed    = 0;
+    last_daq.rvent_lsw_open      = 0;
+    last_daq.rvent_lsw_closed    = 0;
     
-    last_daq->linac_current_open  = 0;
-    last_daq->linac_current_close = 0;
-    last_daq->linac_lsw_extend    = 0;
-    last_daq->linac_lsw_retract   = 0;
+    last_daq.linac_current_open  = 0;
+    last_daq.linac_current_close = 0;
+    last_daq.linac_lsw_extend    = 0;
+    last_daq.linac_lsw_retract   = 0;
 //set labels
     // loop over the columns:
     const char* labels[] = {"P1:000 P2:000 P3:000", "I1:000 I2:000 M:0000", "V1:CLS V2:CLS V3:CLS", "AC:EXT                 "};
@@ -73,8 +74,8 @@ void lcd_update(daq_holder_t* daq, LiquidCrystal lcd){
     if(daq->pressure2 > 999) daq->pressure1 = 999;
     if(daq->pressure3 > 999) daq->pressure1 = 999;
     
-    if(daq->pressure1 != last_daq->pressure1) {
-      //last_daq->pressure1 = daq->pressure1;
+    if(daq->pressure1 != last_daq.pressure1) {
+        last_daq.pressure1 = daq->pressure1;
         lcd.setCursor(3, 0);
         lcd.write("000");
         char temp1[] = "001";
@@ -82,32 +83,32 @@ void lcd_update(daq_holder_t* daq, LiquidCrystal lcd){
         lcd.write(strcpy(temp1, convert(daq->pressure1)));     
     }
 
-    if(daq->pressure2 != last_daq->pressure2) {
-        //last_daq->pressure2 = daq->pressure2;
+    if(daq->pressure2 != last_daq.pressure2) {
+        last_daq.pressure2 = daq->pressure2;
         lcd.setCursor(10, 0);
         lcd.write("000");
         char temp2[] = "001";
         lcd.setCursor(10+moveCursor(daq->pressure2), 0);
         lcd.write(strcpy(temp2, convert(daq->pressure2))); 
     }
-    if(daq->pressure3 != last_daq->pressure3) {
-      //last_daq->pressure3 = daq->pressure3;
+    if(daq->pressure3 != last_daq.pressure3) {
+        last_daq.pressure3 = daq->pressure3;
         lcd.setCursor(17, 0);
         lcd.write("000");      
         char temp3[] = "111";
         lcd.setCursor(17+moveCursor(daq->pressure3), 0);
         lcd.write(strcpy(temp3, convert(daq->pressure3))); 
     }
-    if(daq->ign_pri_current != last_daq->ign_pri_current) {
-        //last_daq->pressure2 = daq->pressure2;
+    if(daq->ign_pri_current != last_daq.ign_pri_current) {
+        last_daq.pressure2 = daq->pressure2;
         lcd.setCursor(3, 1);
         lcd.write("000");
         char temp4[] = "001";
         lcd.setCursor(3+moveCursor(daq->ign_pri_current), 1);
         lcd.write(strcpy(temp4, convert(daq->ign_pri_current))); 
     }
-    if(daq->ign_sec_current != last_daq->ign_sec_current) {
-        //last_daq->pressure2 = daq->pressure2;
+    if(daq->ign_sec_current != last_daq.ign_sec_current) {
+        last_daq.pressure2 = daq->pressure2;
         lcd.setCursor(10, 1);
         lcd.write("000");
         char temp5[] = "001";
@@ -115,7 +116,7 @@ void lcd_update(daq_holder_t* daq, LiquidCrystal lcd){
         lcd.write(strcpy(temp5, convert(daq->ign_sec_current))); 
     }
     
-    if(daq->rocket_mass != last_daq->rocket_mass) {
+    if(daq->rocket_mass != last_daq.rocket_mass) {
         lcd.setCursor(16, 1);
         lcd.write("0000");
         char temp6[] = "0001";
