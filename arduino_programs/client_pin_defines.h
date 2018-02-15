fdcefe5f782eafb6cae5188c23a0b505f17a4440
akmorrison <akmorris@edu.uwaterloo.ca>
2018-02-15 10:26:00 -0500

    Change pin definitions for CF3 and SF1

--- arduino_programs/client_pin_defines.h
+++ arduino_programs/client_pin_defines.h
@@ -7,40 +7,37 @@
 //layed out)
 
 //pins for the switches
-#define PIN_SWITCH_REMOTEFILL    1
-#define PIN_SWITCH_REMOTEVENT    2
-#define PIN_SWITCH_ROCKETVALVE   3
-#define PIN_SWITCH_LINACTUATOR   4
-#define PIN_SWITCH_IGNITION_PRI  5
-#define PIN_SWITCH_IGNITION_SEC  6
-#define PIN_SWITCH_IGNITION_FIRE 7
+#define PIN_SWITCH_REMOTEFILL    27
+#define PIN_SWITCH_REMOTEVENT    26
+#define PIN_SWITCH_ROCKETVALVE   4
+#define PIN_SWITCH_LINACTUATOR   24 //for cf, this will be injector valve
+#define PIN_SWITCH_INJECTOR      3
+#define PIN_SWITCH_IGNITION_PRI  23
+#define PIN_SWITCH_IGNITION_SEC  25
+#define PIN_SWITCH_IGNITION_FIRE 22
 
 //pins for the LCD output
-#define PIN_LCD_RS 8
-#define PIN_LCD_EN 9
-#define PIN_LCD_D0 10 //some lcds can run in 4 bit mode (only d4-d7)
-#define PIN_LCD_D1 11 //I dont know if we can do that with this LCD, so
-#define PIN_LCD_D2 12 //we're gonna start with 8 bit mode. TODO, see if
-#define PIN_LCD_D3 13 //we can do this with only 4 pins
-#define PIN_LCD_D4 14
-#define PIN_LCD_D5 15
-#define PIN_LCD_D6 16
-#define PIN_LCD_D7 17
+#define PIN_LCD_RS 40
+#define PIN_LCD_EN 42
+#define PIN_LCD_D4 41
+#define PIN_LCD_D5 43
+#define PIN_LCD_D6 38
+#define PIN_LCD_D7 39
 
 //pins for the status LEDs
 #define PIN_LED_GREEN 18
 #define PIN_LED_RED   19
 
 //pins for the seven segment display
-#define PIN_SEVENSEG_D1 20
-#define PIN_SEVENSEG_D2 21
-#define PIN_SEVENSEG_A  22
-#define PIN_SEVENSEG_B  23
-#define PIN_SEVENSEG_C  24
-#define PIN_SEVENSEG_D  25
-#define PIN_SEVENSEG_E  26
-#define PIN_SEVENSEG_F  27
-#define PIN_SEVENSEG_G  28
-#define PIN_SEVENSEG_DP 29
+#define PIN_SEVENSEG_D1 28
+#define PIN_SEVENSEG_D2 29
+#define PIN_SEVENSEG_A  30
+#define PIN_SEVENSEG_B  31
+#define PIN_SEVENSEG_C  32
+#define PIN_SEVENSEG_D  33
+#define PIN_SEVENSEG_E  34
+#define PIN_SEVENSEG_F  35
+#define PIN_SEVENSEG_G  36
+#define PIN_SEVENSEG_DP 37
 
 #endif //ifndef CLIENT_PIN_DEFINES_H
