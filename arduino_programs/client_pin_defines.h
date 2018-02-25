1a3f7a464028ae528e04c97e0dd4855a33a5569c
akmorrison <akmorris@edu.uwaterloo.ca>
2018-02-25 01:18:04 -0500

    Change pin defines because of new wiring. Ready for UXO SF1

--- arduino_programs/client_pin_defines.h
+++ arduino_programs/client_pin_defines.h
@@ -7,26 +7,26 @@
 //layed out)
 
 //pins for the switches
-#define PIN_SWITCH_REMOTEFILL    27
-#define PIN_SWITCH_REMOTEVENT    26
+#define PIN_SWITCH_REMOTEFILL    33
+#define PIN_SWITCH_REMOTEVENT    35
 #define PIN_SWITCH_ROCKETVALVE   4
-#define PIN_SWITCH_LINACTUATOR   24 //for cf, this will be injector valve
+#define PIN_SWITCH_LINACTUATOR   34 //for cf, this will be injector valve
 #define PIN_SWITCH_INJECTOR      3
-#define PIN_SWITCH_IGNITION_PRI  23
-#define PIN_SWITCH_IGNITION_SEC  25
-#define PIN_SWITCH_IGNITION_FIRE 22
+#define PIN_SWITCH_IGNITION_PRI  32
+#define PIN_SWITCH_IGNITION_SEC  31
+#define PIN_SWITCH_IGNITION_FIRE 30
 
 //pins for the LCD output
-#define PIN_LCD_RS 40
-#define PIN_LCD_EN 42
-#define PIN_LCD_D4 41
-#define PIN_LCD_D5 43
-#define PIN_LCD_D6 38
-#define PIN_LCD_D7 39
+#define PIN_LCD_RS 25
+#define PIN_LCD_EN 24
+#define PIN_LCD_D4 29
+#define PIN_LCD_D5 27
+#define PIN_LCD_D6 28
+#define PIN_LCD_D7 26
 
 //pins for the status LEDs
-#define PIN_LED_GREEN 48
-#define PIN_LED_RED   49
+#define PIN_LED_GREEN 23
+#define PIN_LED_RED   22
 
 //pins for the seven segment display
 #define PIN_SEVENSEG_D1 28
