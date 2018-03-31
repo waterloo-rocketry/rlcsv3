18fc09abfb67b2b414a0c47be2e2198fc97cc71c
akmorrison <akmorris@edu.uwaterloo.ca>
2018-03-31 19:58:26 -0400

    Clean up SevSeg code to reduce memory usage
    Switch some strings into uint8_ts, remove some loops, get rid of master
    object (i have some major issues with the concept of OOP)

--- arduino_programs/SevSeg.h
+++ arduino_programs/SevSeg.h
@@ -14,13 +14,6 @@
 extern "C" {
 #endif
 
-struct SevSeg {
-	char digitPins[MAXNUMDIGITS];//INPUT/OUTPUT
-	char segmentPins[8];//SEGMENT PIN (ON/OFF)
-	char digitCodes[MAXNUMDIGITS][7]; //Array of bytes needed to display the number
-	uint8_t digits[2];
-} sevSegDisplay;
-
 void start_SevSeg ();//function that runs at the beginning. this sets up the display
 void refresh_SevSeg();
 void setNewNum_SevSeg (uint8_t);//sets desired integer
