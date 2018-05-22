d17bc5de5fe9750e54a7eb0ab88bcbd533f6727e
akmorrison <akmorris@edu.uwaterloo.ca>
2018-05-22 18:39:53 -0400

    Turn on DP on 7seg if sd is not working

--- arduino_programs/SevSeg.c
+++ arduino_programs/SevSeg.c
@@ -1,38 +1,35 @@
 #include "SevSeg.h"
+#include "sd_handler.h"
 #ifdef CLIENT
 #include "client_pin_defines.h"
 #else
 #include "tower_pin_defines.h"
 #endif
 //#include <avr/io.h>
-#include<Stdint.h>
-#define BLANK_IDX 11 // Must match with 'digitCodeMap'
-#define DASH_IDX 12
+#include<stdint.h>
 
 //array of segements that must be displayed to display the correct number
-static const uint8_t digitCodeMap[] = {
+static uint8_t digitCodeMap[] = {
   //GFEDCBA  Segments      7-segment map:
-  0b0111111, // 0   "0"          AAA
-  0b0000110, // 1   "1"         F   B
-  0b1011011, // 2   "2"         F   B
-  0b1001111, // 3   "3"          GGG
-  0b1100110, // 4   "4"         E   C
-  0b1101101, // 5   "5"         E   C
-  0b1111101, // 6   "6"          DDD
-  0b0000111, // 7   "7"
-  0b1111111, // 8   "8"
-  0b1101111, // 9   "9"
-  0b1110111, // A "A"
-  0b1111100, // b "b"
-  0b0111001, // C "C"
-  0b1011110, // d "d"
-  0b1111001, // E "E"
-  0b1110001 // F "F"
-  //"0000000", // 32  ' '  BLANK
-  //"0000001", // 45  '-'  DASH
+  0b00111111, // 0   "0"          AAA
+  0b00000110, // 1   "1"         F   B
+  0b01011011, // 2   "2"         F   B
+  0b01001111, // 3   "3"          GGG
+  0b01100110, // 4   "4"         E   C
+  0b01101101, // 5   "5"         E   C
+  0b01111101, // 6   "6"          DDD  DP
+  0b00000111, // 7   "7"
+  0b01111111, // 8   "8"
+  0b01101111, // 9   "9"
+  0b01110111, // A "A"
+  0b01111100, // b "b"
+  0b00111001, // C "C"
+  0b01011110, // d "d"
+  0b01111001, // E "E"
+  0b01110001 // F "F"
 };
 
-const uint8_t * digitCodes[2];
+static uint8_t * digitCodes[2];
 
 static const unsigned digitPinsIn[] = {
     PIN_SEVENSEG_D1,
@@ -45,7 +42,8 @@ static const unsigned segmentPinsIn[] = {
     PIN_SEVENSEG_D,
     PIN_SEVENSEG_E,
     PIN_SEVENSEG_F,
-    PIN_SEVENSEG_G
+    PIN_SEVENSEG_G,
+    PIN_SEVENSEG_DP
 };
 
 //functions
@@ -56,7 +54,7 @@ void start_SevSeg () {//sets the values of the digitpin and segment pins in the 
   pinMode(digitPinsIn[0], OUTPUT);
   pinMode(digitPinsIn[1], OUTPUT);
 
-  for (int x = 0; x < 7; x++) {
+  for (int x = 0; x < 8; x++) {
     pinMode(segmentPinsIn[x], OUTPUT);	//sets digits pins as outputs
     digitalWrite(segmentPinsIn[x], HIGH);	//turns segment pins off
   }
@@ -67,7 +65,7 @@ void refresh_SevSeg() {//refreshes display and lights up the segments
   digitalWrite(digitPinsIn[0], HIGH);//turns first segment pin on
   digitalWrite(digitPinsIn[1], LOW);//turns off second digit
   for (int i = 0; i < 2; i++) { //goes throught each digit code and lights up appriopriate segment for each one
-    for (int j = 0; j < 7; j++) {
+    for (int j = 0; j < 8; j++) {
       digitalWrite(segmentPinsIn[j], (*digitCodes[i] & (1<<j)) ? LOW : HIGH);
     }
     delay(10);//this is so that 2 numbers can be displayed instantaneously
@@ -85,4 +83,16 @@ void setDigitCodes(uint8_t numToShow) { //this function gets the digit code of e
   // Set the digitCode for each digit in the display(digit array)
   digitCodes[0] = digitCodeMap + (numToShow & 0xF);
   digitCodes[1] = digitCodeMap + ((numToShow >> 4) & 0xF);
+
+  //if sd card is not active, turn on the decimal points
+  if(!sd_active())
+  {
+    *digitCodes[0] |= 0x80;
+    *digitCodes[1] |= 0x80;
+  }
+  else
+  {
+    *digitCodes[0] &= ~(0x80);
+    *digitCodes[1] &= ~(0x80);
+  }
 }
