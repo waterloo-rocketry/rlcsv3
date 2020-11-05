5dd3db5f1ec4897fcd37e361e72df866c6860039
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2020-11-04 19:47:38 -0500

    Add multi-byte slave writes and sending sensor data

--- src/relay_pic/i2c.c
+++ src/relay_pic/i2c.c
@@ -1,4 +1,5 @@
 #include"i2c.h"
+#include"relay_general.h"
 
 uint16_t i2cSlaveRecv; // Data received on i2c
 uint16_t i2cSlaveSend; // Data to be sent on i2c
@@ -17,45 +18,60 @@ void i2c_slave_init(uint16_t address) {
 }
 
 void i2c_handle_interrupt(void) {
-     uint16_t temp;
-        SSPCONbits.CKP = 0;
-       
-        // If overflow or collision.
-        if ((SSPCONbits.SSPOV) || (SSPCONbits.WCOL)) {
-            temp = SSPBUF; // Read the previous value to clear the buffer
-            SSPCONbits.SSPOV = 0; // Clear the overflow flag
-            SSPCONbits.WCOL = 0;   // Clear the collision bit
-            SSPCONbits.CKP = 1;
-        }
+    uint16_t temp;
+    SSPCONbits.CKP = 0;
 
-        // If last byte was Address + write
-        if (!SSPSTATbits.D_nA && !SSPSTATbits.R_nW) {
-            while(!BF);
-            temp = SSP1BUF;
-            //while(!BF);
-            BF = 0;
-            SSPCONbits.CKP = 1;
-            while(!BF);
-            i2cSlaveRecv = SSP1BUF;
-            if(i2cSlaveRecv == 2) {
-                set_led_off();
-            } else {
-                set_led_on();
-            }
-            SSPCONbits.CKP = 1;
-            BF = 0;
-            //SSPM3 = 0;
-            SSP1IF = 0;
-       }
-        
-       // If last byte was Address + read
-       else if(!SSPSTATbits.D_nA && SSPSTATbits.R_nW) {
-           temp = SSPBUF;
-           BF = 0;
-           SSPBUF = i2cSlaveSend;
-           SSPCONbits.CKP = 1;
-           while(SSPSTATbits.BF);
-       }
-       
-       SSP1IF = 0;
+    SSP1IF = 0;
+
+    // If overflow or collision.
+    if ((SSPCONbits.SSPOV) || (SSPCONbits.WCOL)) {
+        temp = SSPBUF; // Read the previous value to clear the buffer
+        SSPCONbits.SSPOV = 0; // Clear the overflow flag
+        SSPCONbits.WCOL = 0;   // Clear the collision bit
+        SSPCONbits.CKP = 1;
+        return;
+    }
+
+    // If last byte was Address + write
+    if (!SSPSTATbits.D_nA && !SSPSTATbits.R_nW) {
+        while(!BF);
+        temp = SSP1BUF;
+        BF = 0;
+        SSPCONbits.CKP = 1;
+        while(!BF);
+        i2cSlaveRecv = SSP1BUF;
+        if (i2cSlaveRecv == 2) {
+            set_select_on();
+        } else {
+            set_select_off();
+        }
+        SSPCONbits.CKP = 1;
+        BF = 0;
+        // We already set this above but for some reason we need to set it here too
+        SSP1IF = 0;
+    }
+    // If this is a read
+    else if (SSPSTATbits.R_nW) {
+        static uint8_t read_pointer;
+        // If this is the first byte in a read
+        if (!SSPSTATbits.D_nA) {
+            read_pointer = 0;
+        } 
+        temp = SSPBUF;
+        BF = 0;
+        if (read_pointer == 0) {
+            SSPBUF = (get_lim2() << 1) | get_lim1();
+        } else if (read_pointer == 1) {
+            SSPBUF = (uint8_t)(analog_inputs[0]);
+        } else if (read_pointer == 2) {
+            SSPBUF = (uint8_t)(analog_inputs[0] >> 8);
+        } else if (read_pointer == 3) {
+            SSPBUF = (uint8_t)(analog_inputs[1]);
+        } else if (read_pointer == 4) {
+            SSPBUF = (uint8_t)(analog_inputs[1] >> 8);
+        }
+        read_pointer++;
+        SSPCONbits.CKP = 1;
+        while(SSPSTATbits.BF);
+    }
 }
