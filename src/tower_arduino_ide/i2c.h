70f9dade14708a132a90233090de3a2c65b0af3b
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2021-03-09 01:45:30 -0500

    Add towerside i2c functions

---
+++ src/tower_arduino_ide/i2c.h
@@ -0,0 +1,29 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "shared_types.h"
+#include <stdbool.h>
+
+typedef enum {
+  I2C_VALVE_1 = 1,
+  I2C_VALVE_2 = 2,
+  I2C_VALVE_3 = 3,
+  I2C_VALVE_4 = 4,
+  I2C_VALVE_INJECTOR = 5,
+  I2C_IGNITION = 6
+} valve_id_t;
+
+void i2c_init(void);
+void i2c_set_valve_power(valve_id_t valve, bool value);
+void i2c_set_valve_select(valve_id_t valve, bool value);
+valve_state_t i2c_get_state(valve_id_t valve);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ifndef I2C_H_
