d8f484496bd33555126d9c217f37b85e391028f3
KavinSatheeskumar <kavinsathees@gmail.com>
2022-12-29 19:03:35 -0500

    Add basic encoding and decoding protocols (start and end bytes)

--- src/common/communication.hpp
+++ src/common/communication.hpp
@@ -9,19 +9,27 @@
 template<typename ST, typename RT>
 class Communicator {
 	Stream &stream;
-	uint8_t receive_buffer[sizeof(RT)];
+	// Note, buffer size will depend on 
+	// the encoding and decoding protocol used
+	uint8_t receive_buffer[sizeof(RT) + 2];
+
 	int buffer_position = 0;
-	uint16_t time_since_last_byte = 0;
+	unsigned long time_since_last_byte = 0;
 	public:
 	Communicator(Stream &stream): stream{stream} {}
 
 	// loops through send_data and sends each byte
 	bool send(ST *send_data) {
+		/*
+		Note, encoding protocol may varry
+		*/
 		bool status = true;
 		uint8_t *byte = reinterpret_cast<uint8_t*>(send_data);
+		stream.write('W');
 		for (int i = 0; i < sizeof(ST); ++i) {
 			status &= stream.write(send_data + i);
 		}
+		stream.write('R');
 		return status;
 	}
 
@@ -31,7 +39,15 @@ class Communicator {
 			return nullptr;
 		}
 
-		memcpy(dest, receive_buffer, sizeof(RT));
+		/*
+		Note, error checking and decoding protocal may varry
+		*/
+
+		if (buffer_position[0] != 'W' || buffer_position[sizeof(RT) + 1] != 'R') {
+			return nullptr;
+		}
+
+		memcpy(dest, receive_buffer + 1, sizeof(RT));
 
 		buffer_position = 0;
 		return dest;
@@ -39,7 +55,9 @@ class Communicator {
 
 	bool read_byte() {
 		if (stream.available()) {
-			receive_buffer[buffer_position++] = static_cast<uint8_t>(stream.read());
+			if (buffer_position < sizeof(RT)) {
+				receive_buffer[buffer_position++] = static_cast<uint8_t>(stream.read());
+			}
 			time_since_last_byte = millis();
 			return true;
 		} else if (millis() - time_since_last_byte > Config::MAX_DELAY_BETWEEN_BYTES) {
