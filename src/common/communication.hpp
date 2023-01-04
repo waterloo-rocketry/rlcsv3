c9dda0624c3f990619ec047182b01cf7f416b908
KavinSatheeskumar <kavinsathees@gmail.com>
2023-01-03 23:02:27 -0500

    Rename a bunch of things, and add an inline method

--- src/common/communication.hpp
+++ src/common/communication.hpp
@@ -4,9 +4,6 @@
 #include <stdint.h>
 #include <cstring>
 #include "mock_arduino.hpp"
-#include "config.hpp"
-
-#include <iostream>
 
 template<typename ST, typename RT>
 class Communicator {
@@ -16,33 +13,25 @@ class Communicator {
 	static const size_t BUFF_SIZE = sizeof(RT) + 2;
 	uint8_t receive_buffer[BUFF_SIZE];
 
-	int buffer_position = 0;
+	size_t buffer_position = 0;
 	unsigned long time_of_last_byte = 0;
-	const uint16_t max_delay_between_bytes;
-	public:
-	Communicator(Stream &stream, uint16_t max_delay_between_bytes = 300):
+	const unsigned long max_delay_between_bytes;
+public:
+	Communicator(Stream &stream, unsigned long max_delay_between_bytes = 300):
 		stream{stream},
 		max_delay_between_bytes{max_delay_between_bytes} {}
 
 	// loops through send_data and sends each byte
 	bool send(ST &send_data) {
 		bool status = true;
-		uint8_t *byte = reinterpret_cast<uint8_t*>(&send_data);
-
-		/*
-		Note, if you ever wanna send data with different encoding 
-		and decoding protocols, just specalize this template.
-		*/
+		uint8_t *send_data_uint8 = reinterpret_cast<uint8_t*>(&send_data);
 
 		stream.write('W');
-		for (int i = 0; i < sizeof(ST); ++i) {
-			status &= stream.write(*(byte + i));
-		}
+		stream.write(send_data_uint8, sizeof(ST));
 		stream.write('R');
 		return status;
 	}
 
-	// returns nullptr on failure, 
 	bool get_message(RT *dest) {
 		if (buffer_position < BUFF_SIZE) {
 			return false;
@@ -72,6 +61,8 @@ class Communicator {
 
 		return false;
 	}
+
+	inline uint8_t seconds_since_last_contact() {return millis() - time_of_last_byte;}
 };
 
 #endif
