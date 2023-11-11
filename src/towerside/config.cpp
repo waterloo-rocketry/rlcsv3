feba5e5de10832246e2b5e43754dbfd68755c424
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2023-11-11 16:47:49 -0500

    Update LSF2 proposed safe state
    Update clientside ignition current display format

--- src/towerside/config.cpp
+++ src/towerside/config.cpp
@@ -9,24 +9,20 @@ struct Actuators {
   actuator::I2C valve_1{1};
   actuator::I2C valve_2{2};
   actuator::I2C valve_3{3};
-  actuator::I2C valve_4{4};
-  //actuator::I2C injector_valve{5};
+  actuator::I2C vent_valve{4};
+  actuator::I2C injector_valve{5};
   actuator::Ignition ignition_primary{6};
   actuator::Ignition ignition_secondary{7};
-  actuator::I2C fill_disconnect{5};
-  actuator::I2C rocket_power {8};
 } ACTUATORS;
 
 void apply(const ActuatorMessage &command) {
   ACTUATORS.valve_1.set(command.valve_1);
   ACTUATORS.valve_2.set(command.valve_2);
   ACTUATORS.valve_3.set(command.valve_3);
-  ACTUATORS.valve_4.set(command.valve_4);
-  //ACTUATORS.injector_valve.set(command.injector_valve);
+  ACTUATORS.vent_valve.set(command.vent_valve);
+  ACTUATORS.injector_valve.set(command.injector_valve);
   ACTUATORS.ignition_primary.set(command.ignition_primary);
   ACTUATORS.ignition_secondary.set(command.ignition_primary); // fire both ignitions in response to ignition_primary
-  ACTUATORS.fill_disconnect.set(command.fill_disconnect);
-  ACTUATORS.rocket_power.set(!command.rocket_power); // the firmware inverts the command, so un-invert it here
 }
 
 SensorMessage build_sensor_message() {
@@ -41,9 +37,6 @@ SensorMessage build_sensor_message() {
       .valve_1_state = ACTUATORS.valve_1.get_state(),
       .valve_2_state = ACTUATORS.valve_2.get_state(),
       .valve_3_state = ACTUATORS.valve_3.get_state(),
-      .valve_4_state = ACTUATORS.valve_4.get_state(),
-      .fill_disconnect_state = ACTUATORS.fill_disconnect.get_state(),
-      //.injector_valve_state = ACTUATORS.injector_valve.get_state(),
   };
 }
 
