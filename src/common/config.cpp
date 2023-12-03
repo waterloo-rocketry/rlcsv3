2fbc748a87668b0c2cb7781ee4236d9db3621fea
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2023-12-03 08:59:49 -0500

    Change VA2 safe state to open for LSF2

--- src/common/config.cpp
+++ src/common/config.cpp
@@ -3,7 +3,7 @@
 ActuatorMessage build_safe_state(const ActuatorMessage &current_state) {
   return ActuatorMessage{
       .valve_1 = false,
-      .valve_2 = false,
+      .valve_2 = true,
       .valve_3 = false,
       .vent_valve = true,
       .injector_valve = current_state.injector_valve,
