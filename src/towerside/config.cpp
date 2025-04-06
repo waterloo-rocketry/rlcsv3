31431aa0beac3374c582cb0aac3bb1f8acfa3241
unknown <kavinsatheesk@gmail.com>
2025-04-06 17:12:55 -0400

    forgot a file

--- src/towerside/config.cpp
+++ src/towerside/config.cpp
@@ -26,8 +26,8 @@ void apply(const ActuatorMessage &command) {
   ACTUATORS.NV105.set(!command.NV105);
   ACTUATORS.OV102.set(!command.OV102);
   ACTUATORS.FV102.set(!command.FV102);
-  ACTUATORS.OV101v.set(!command.OV101v);
-  ACTUATORS.FV101v.set(!command.FV101v);
+  ACTUATORS.OV101v.set(command.OV101v);
+  ACTUATORS.FV101v.set(command.FV101v);
   ACTUATORS.ignition.set(command.ignition);
   seven_seg::display(command);
 }
