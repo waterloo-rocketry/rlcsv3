b95feafb07727d8a8bf2d7058e20fc7f39c894bf
dare-eee-enn <darienhyng1@gmail.com>
2026-01-31 23:49:21 -0500

    making pr

--- src/towerside/config.cpp
+++ src/towerside/config.cpp
@@ -20,13 +20,13 @@ struct Actuators {
 } ACTUATORS;
 
 void apply(const ActuatorMessage &command) {
-  ACTUATORS.ov101.set(command.ov101);
-  ACTUATORS.ov102.set(command.ov102);
+  ACTUATORS.ov101.set(!command.ov101);
+  ACTUATORS.ov102.set(!command.ov102);
   ACTUATORS.ov301.set(!command.ov301);
-  ACTUATORS.nv201.set(command.nv201);
-  ACTUATORS.cdv401.set(command.cdv401);
+  ACTUATORS.nv201.set(!command.nv201);
+  ACTUATORS.cdv401.set(!command.cdv401);
   ACTUATORS.qd301.set(!command.qd301);
-  ACTUATORS.ov302.set(command.ov302);
+  ACTUATORS.ov302.set(!command.ov302);
   ACTUATORS.ignition_primary.set(command.ignition_primary);
   ACTUATORS.ignition_secondary.set(command.ignition_primary); // fire both ignitions in response to ignition_primary
   ACTUATORS.heater_1.set(command.tank_heating_1);
